28f118c61a4efe47ee2b2c6cb23bf5a38d6538cd
Nikita Teslyuk <teslyuk.nikita@gmail.com>
2023-11-24 14:03:30 +0400

    Mongo database up

--- bot/config.py
+++ bot/config.py
@@ -8,5 +8,9 @@ config_dir = Path(__file__).parent.parent.resolve() / "config"
 with open(config_dir / "config.yml", "r") as f:
     config_yaml = yaml.safe_load(f)
 
+# load .env config
+config_env = dotenv.dotenv_values(config_dir / "config.env")
+
 # config parameters
 telegram_token = config_yaml["telegram_token"]
+mongodb_uri = f"mongodb://mongo:{config_env['MONGODB_PORT']}"
